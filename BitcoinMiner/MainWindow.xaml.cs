806e0f048979233625fbd7d923aeea062d8a4cc0
Lander Vanhove <11402211@student.pxl.be>
2023-11-14 10:14:37 +0100

    BM-10: Investeringsknoppen disabled en enkel enabled/zichtbaar als er genoeg BTC is.

--- BitcoinMiner/MainWindow.xaml.cs
+++ BitcoinMiner/MainWindow.xaml.cs
@@ -20,18 +20,25 @@ namespace BitcoinMiner
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        int aantalBTC = 0;
+        double aantalBTC = 0;
+        double kostBasic = 15;
+        double kostAdvanced = 100;
+        double kostMiningRig = 1100;
+        double kostQuantum = 12000;
      
         private void ImgBTC_MouseDown(object sender, MouseButtonEventArgs e)
         {
             ImgBTC.Height = 230;
-            aantalBTC++;
+            aantalBTC+= 15;
             LblBTC.Content = aantalBTC.ToString();
             this.Title = $"You have mined {aantalBTC} BTC so far!";
+            CheckStoreAvailability();
         }
         private void ImgBTC_MouseUp(object sender, MouseButtonEventArgs e)
         {
@@ -41,6 +48,34 @@ namespace BitcoinMiner
         private void ImgBTC_MouseLeave(object sender, MouseEventArgs e)
         {
             ImgBTC.Height = 210;
+        }
+
+        private void CheckStoreAvailability()
+        {
+            if (aantalBTC >= kostBasic)
+            {
+                GridBasic.Opacity = 1;
+                GridBasic.IsEnabled = true;
+                BorderBasic.BorderThickness = new Thickness(2);
+            }
+            if (aantalBTC >= kostAdvanced)
+            {
+                GridAdvanced.Opacity = 1;
+                GridAdvanced.IsEnabled = true;
+                BorderAdvanced.BorderThickness = new Thickness(2);
+            }
+            if (aantalBTC >= kostMiningRig)
+            {
+                GridMiningRig.Opacity = 1;
+                GridMiningRig.IsEnabled = true;
+                BorderMiningRig.BorderThickness = new Thickness(2);
+            }
+            if (aantalBTC >= kostQuantum)
+            {
+                GridQuantum.Opacity = 1;
+                GridQuantum.IsEnabled = true;
+                BorderQuantum.BorderThickness = new Thickness(2);
+            }
         }
     }
 }
